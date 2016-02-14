2ba9db40cf87e22b64ccaa2b55e794e34b04d91e
Sarn Wattanasri <sarn@Sarns-MacBook-Pro.local>
2016-02-13 22:52:35 -0800

    Added the infinite scroll first time

--- Twitter/Tweet.swift
+++ Twitter/Tweet.swift
@@ -45,7 +45,6 @@ class Tweet: NSObject {
         let formatter = NSDateFormatter()
         formatter.dateFormat = "EEE MMM d HH:mm:ss Z y"
         createdAt = formatter.dateFromString(createdAtString!)
-
     }
     
     class func tweetsWithArray(array: [NSDictionary]) -> [Tweet] {
--- Twitter/TweetsViewController.swift
+++ Twitter/TweetsViewController.swift
@@ -15,6 +15,103 @@ class TweetsViewController: UIViewController, UITableViewDataSource, UITableView
     
     var tweets: [Tweet]?
     
+    var refreshControl: UIRefreshControl!
+    
+    
+    //flag for infinite scroll
+    var isMoreDataLoading = false
+    var loadingMoreView: InfiniteScrollActivityView?
+    var selectedCategories: [String]?
+    
+    var loadMoreOffset = 20
+    
+    
+
+    
+    //infinite scroll
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        if !isMoreDataLoading {
+            //calculate the position of one screen length before the bottom of the results
+            let scrollViewContentHeight = tableView.contentSize.height
+            let scrollOffsetThreshold = scrollViewContentHeight - tableView.bounds.size.height
+            
+            //when the user has scrolled past the threshold, start requesting
+            if scrollView.contentOffset.y > scrollOffsetThreshold && tableView.dragging {
+                isMoreDataLoading = true
+                
+                let frame = CGRectMake(0, tableView.contentSize.height, tableView.bounds.size.width, InfiniteScrollActivityView.defaultHeight)
+                loadingMoreView?.frame = frame
+                loadingMoreView!.startAnimating()
+                
+                //load more data
+                loadMoreData()
+            }
+        }
+    }
+    
+    @available(iOS, deprecated=8.0) //suppress warning on deprecated GET from BDBOAuth1Manager's GET method
+    func loadMoreData() {
+        
+        //call Twitter API to load the next set of data
+        let twitterApiParams = [ "since_id": self.loadMoreOffset, "count": 20 ]
+        TwitterClient.sharedInstance.homeTimelineWithParams(twitterApiParams) { tweets, error in
+            
+            if error != nil {
+                self.delay(2.0, closure: {
+                    self.loadingMoreView?.stopAnimating()
+                    //TODO: show network error
+                })
+            } else {
+                self.delay(0.5, closure: { Void in
+                    self.loadMoreOffset += 20
+                    self.tweets?.appendContentsOf(tweets!)
+                    self.tableView.reloadData()
+                    self.loadingMoreView?.stopAnimating()
+                    self.isMoreDataLoading = false
+                })
+            }
+            
+        }
+    }
+    
+    func setupInfiniteScrollView() {
+        let frame = CGRectMake(0, tableView.contentSize.height,
+            tableView.bounds.size.width,
+            InfiniteScrollActivityView.defaultHeight
+        )
+        loadingMoreView = InfiniteScrollActivityView(frame: frame)
+        loadingMoreView!.hidden = true
+        tableView.addSubview( loadingMoreView! )
+        
+        var insets = tableView.contentInset
+        insets.bottom += InfiniteScrollActivityView.defaultHeight
+        tableView.contentInset = insets
+    }
+    
+    //pull to refresh
+    func pullToRefreshControl() {
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
+    }
+    
+    func onRefresh() {
+        delay(5, closure: {
+            self.refreshControl.endRefreshing()
+        })
+    }
+    
+    func delay(delay: Double, closure: () -> () ) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure
+        )
+    }
+    
+    
     @available(iOS, deprecated=8.0) //suppress warning on deprecated GET from BDBOAuth1Manager's GET method
     override func viewDidLoad() {
         
@@ -24,12 +121,14 @@ class TweetsViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.dataSource = self
         
         // Do any additional setup after loading the view.
-        TwitterClient.sharedInstance.homeTimelineWithParams(nil) { tweets, error in
+        let twitterApiParams = [ "since_id": 20, "count": 20 ]
+        TwitterClient.sharedInstance.homeTimelineWithParams(twitterApiParams) { tweets, error in
             self.tweets = tweets
             self.tableView.reloadData()
         }
         
-        
+        pullToRefreshControl()
+        setupInfiniteScrollView()
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
